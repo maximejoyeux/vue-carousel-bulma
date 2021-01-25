b6ff318330750cdeb77086d5c688b82ea03cfa19
maximejoyeux <maxime.mjoyeux@gmail.com>
2021-01-25 17:22:20 +0100

    init vue files

---
+++ src/App.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <carousel
+            :items="images"
+            :loop="true"
+            :infinite="true"
+            :slidesToShow="3"
+            :slidesToScroll="1"
+            :pagination="false"
+            :navigation="false"
+            :navigationSwipe="true"
+            :autoplay="true"
+            :breakpoints="[]"
+        >
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import carousel from './components/bulma-vue-carousel.vue';
+
+export default {
+  name: 'app',
+  components: {
+    carousel,
+  },
+  data() {
+    return {
+      images: [
+        {
+          title: 'The best image1',
+          src: './src/assets/images/sports.1.jpg',
+          desc:
+            'Laboris ullamco nostrud qui consectetur aute eiusmod irure aliquip veniam aliquip ut officia laboris amet. Consequat id excepteur mollit dolor fugiat labore. Commodo nulla minim eu commodo reprehenderit veniam eu aliqua aute cillum proident nostrud veniam. Anim voluptate nulla adipisicing in ullamco irure. Velit nostrud ad in duis ex minim nulla cupidatat duis esse nostrud ex ad exercitation. Duis ut do laborum commodo Lorem sunt aliqua labore ex occaecat aliqua nulla id.',
+          other: ``,
+        },
+        {
+          title: 'The best image2',
+          src: './src/assets/images/sports.2.jpg',
+          desc:
+            'Non fugiat veniam ipsum Lorem anim sit culpa Lorem. Tempor culpa enim adipisicing nostrud exercitation cillum ex dolore dolor mollit irure enim velit commodo. Labore veniam esse incididunt consequat voluptate esse veniam deserunt excepteur. Lorem labore aute labore voluptate. Enim esse eu consectetur ex fugiat aliqua. Quis dolor amet nostrud ea.',
+          other: ``,
+        },
+        {
+          title: 'The best image3',
+          src: './src/assets/images/sports.3.jpg',
+          desc:
+            'Ex in amet amet ea tempor nisi in commodo ullamco exercitation ea qui eiusmod.',
+          other: ``,
+        },
+        {
+          title: 'The best image4',
+          src: './src/assets/images/sports.5.jpg',
+          desc:
+            'Sunt amet nisi non non excepteur amet occaecat nulla occaecat deserunt excepteur. Culpa occaecat nulla nulla ea ea. Non occaecat veniam elit in commodo ex esse ipsum occaecat cillum fugiat consequat. Aliquip laborum cupidatat elit in commodo commodo ea. Id esse labore adipisicing proident adipisicing anim sint exercitation nostrud velit. Veniam aliqua magna enim do.',
+          other: ``,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+
+<style lang="scss">
+@import '~node_modules/bulma-carousel/dist/css/bulma-carousel.min.css';
+
+// CUSTOM CARROUSEL STYLE
+.slider-navigation {
+  font-size: 20px;
+  background: transparent;
+  position: absolute;
+  top: 50%;
+  margin-top: -20px;
+  cursor: pointer;
+  @media only screen and (max-width: 768px) {
+    display: none;
+  }
+}
+.slider-navigation.previous {
+  left: -2.5%;
+}
+.slider-navigation.next {
+  right: -2.5%;
+}
+.slider-navigation-previous,
+.slider-navigation-next {
+  display: none;
+}
+// Arrows
+.icon-left-open-big,
+.icon-right-open-big {
+  font-size: 20px;
+}
+// END CUSTOM CARROUSEL STYLE
+</style>
---
+++ src/components/bulma-vue-carousel.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="container">
+    <div ref="carousel" class="carousel is-clipped">
+      <div class="slide" v-for="(item, index) in items" :key="item.title">
+        <slot name="item" :item="item" :index="index"></slot>
+      </div>
+    </div>
+    <a class="carousel-arrow" @click="carousel.previous()">
+      <slot name="previous"></slot>
+    </a>
+    <a class="carousel-arrow" @click="carousel.next()">
+      <slot name="next"></slot>
+    </a>
+  </div>
+</template>
+
+<script>
+import BulmaCarousel from 'bulma-carousel';
+
+export default {
+  props: {
+    items: [Object, Array],
+    initialSlide: {
+      type: Number,
+      required: false,
+      default: 0,
+    },
+    slidesToScroll: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+    slidesToShow: {
+      type: Number,
+      required: false,
+      default: 1,
+    },
+    navigation: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    navigationKeys: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    navigationSwipe: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    pagination: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    loop: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    infinite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    effect: {
+      type: String,
+      required: false,
+      default: 'translate',
+    },
+    duration: {
+      type: Number,
+      required: false,
+      default: 300,
+    },
+    timing: {
+      type: String,
+      required: false,
+      default: 'ease',
+    },
+    autoplay: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    autoplaySpeed: {
+      type: Number,
+      required: false,
+      default: 3000,
+    },
+    pauseOnHover: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    breakpoints: {
+      type: Array,
+      required: false,
+      default: [],
+    },
+  },
+  data() {
+    return {
+      carousel: null,
+      customClass: null,
+    };
+  },
+  mounted() {
+    const tag = this.$refs.carousel;
+
+    this.carousel = new BulmaCarousel(tag, {
+      slidesToScroll: this.slidesToScroll,
+      slidesToShow: this.slidesToShow,
+      loop: this.loop,
+      initialSlide: this.initialSlide,
+      slidesToScroll: this.slidesToScroll,
+      slidesToShow: this.slidesToShow,
+      navigation: this.navigation,
+      navigationKeys: this.navigationKeys,
+      navigationSwipe: this.navigationSwipe,
+      pagination: this.pagination,
+      loop: this.loop,
+      infinite: this.infinite,
+      effect: this.effect,
+      duration: this.duration,
+      timing: this.timing,
+      autoplay: this.autoplay,
+      autoplaySpeed: this.autoplaySpeed,
+      pauseOnHover: this.pauseOnHover,
+      breakpoints: this.breakpoints,
+    });
+
+    this.$on('hook:beforeDestroy', this.destroyCarousel);
+  },
+  methods: {
+    destroyCarousel() {
+      if (!this.carousel) return;
+
+      this.carousel.destroy();
+      this.carousel = null;
+    },
+  },
+  beforeDestroy() {
+    this.carousel.destroy();
+    this.carousel = null;
+  },
+};
+</script>
